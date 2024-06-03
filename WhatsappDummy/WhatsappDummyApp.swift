507777a32b9757ebe963629baa08137be930a9fa
Abhay Mundhara <abhay.mundhara@gmail.com>
2024-06-03 18:41:48 +0100

    Initial Commit

---
+++ WhatsappDummy/WhatsappDummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsappDummyApp.swift
+//  WhatsappDummy
+//
+//  Created by abhay mundhara on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhatsappDummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
